4fa24987f70bb59521e6e71cde77e754e13c4800
Camille Baillat <camille.baillat@protonmail.com>
2018-06-14 10:01:36 +0200

    Removed the route nesting while working on the CRUD actions of the messages

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    resources :messages
-  end
+  resources :users
+  resources :messages
 
   root 'welcome#index'
 end
